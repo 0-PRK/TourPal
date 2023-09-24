b4a630a5058b87ab92b49f578c16a6a5e13236bc
Mohit Rajbhandari <rbmohit1283@gmail.com>
2023-09-24 14:37:18 +0545

    changes to landing

--- frontend/src/Components/landing.js
+++ frontend/src/Components/landing.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet"></link>
 
 const LandingPage = () => {
